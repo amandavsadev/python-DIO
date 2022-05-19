76f8d91d5fa0d2130b35c4e1ba4be247ede41dea
Amanda Amorim <amandavsadev@gmail.com>
2022-05-19 11:10:40 -0300

    11/05

---
+++ MaiorTres.py
@@ -0,0 +1,20 @@
+# Função do programa: Mostrar qual dos 3 números é maior.
+# Autora: Amanda Amorim
+
+a = int(input('Primeiro Valor: '))
+b = int(input('Segundo Valor: '))
+c = int(input('Terceiro valor: '))
+
+# Condicional if(se) e operador lógico and
+if a > b and a > c:
+    print('O maior valor é {}'.format(a))
+# em python, conseguimos definir o início e o fim de um bloco através da identação.
+
+# else if
+elif b > a and b > c:
+    print('O maior número é {}'.format(b))
+
+else:
+    print('O maior número é {}'.format(c))
+
+print('final do programa')
